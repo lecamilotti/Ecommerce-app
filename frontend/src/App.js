75756786725d42cc172fad15fdc7baee2d0f1335
Leandro <lecamilotti@gmail.com>
2021-05-15 11:22:35 +0200

    add new pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Product from './components/Product';
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 
 function App() {
   return (
-    <div className="grid-container">
+    <BrowserRouter>
+        <div className="grid-container">
       <header className="row">
         <div>
           <a className="brand" href="/">EGPU Dock</a>
@@ -18,21 +21,17 @@ function App() {
 
       </header>
       <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <Product key={product._id} product={product}></Product>
-            
-         ))}
-          
-        </div>
+        <Route path="/product/:id" component={ProductScreen}></Route>
+        <Route path="/" component={HomeScreen} exact></Route>
+        
       </main>
       <footer className="row center">
         All right reserverd Leandro Camilotti Web Developer
         </footer>
 
     </div>
-
-
+  
+    </BrowserRouter>
   );
 }
 
