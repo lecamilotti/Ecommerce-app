3e4f3dbccae66dc9ca0549c6aad9d9ffb0cf503e
Leandro <lecamilotti@gmail.com>
2021-05-16 11:31:04 +0200

    cart screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,17 +1,20 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers';
+import {
+  productDetailsReducer,
+  productListReducer,
+} from './reducers/productReducers';
 
 const initialState = {};
 const reducer = combineReducers({
-    productList: productListReducer,
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
-const composeEnhancer = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-    reducer, 
-    initialState,
-    composeEnhancer(applyMiddleware(thunk))
-     );
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
 
 export default store;
