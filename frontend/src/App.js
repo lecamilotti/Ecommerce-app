6ebea74de9f2122448dc54717726f69e9e327bd5
Leandro <lecamilotti@gmail.com>
2021-05-17 13:21:50 +0200

    create account page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import SigninScreen from './screens/SigninScreen';
 
 function App() {
@@ -16,14 +17,13 @@ function App() {
   const signoutHandler = () => {
     dispatch(signout());
   };
-
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
             <Link className="brand" to="/">
-              EGPU Dock Station
+              amazona
             </Link>
           </div>
           <div>
@@ -35,26 +35,30 @@ function App() {
             </Link>
             {userInfo ? (
               <div className="dropdown">
-              <Link to="#">
-                {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
                 </Link>
                 <ul className="dropdown-content">
-                  <Link to="#signout" onClick={signoutHandler}>Sign Out</Link>
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
                 </ul>
-                </div>
+              </div>
             ) : (
               <Link to="/signin">Sign In</Link>
             )}
-            
           </div>
         </header>
         <main>
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
-        <footer className="row center">Leandro Camilotti Web Developer Junior</footer>
+        <footer className="row center">All right reserved</footer>
       </div>
     </BrowserRouter>
   );
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,13 +5,16 @@ import {
   productDetailsReducer,
   productListReducer,
 } from './reducers/productReducers';
-import { userSigninReducer } from './reducers/userReducer';
+import {
+  userRegisterReducer,
+  userSigninReducer,
+} from './reducers/userReducers';
 
 const initialState = {
   userSignin: {
     userInfo: localStorage.getItem('userInfo')
-    ? JSON.parse(localStorage.getItem('userInfo'))
-    : null,
+      ? JSON.parse(localStorage.getItem('userInfo'))
+      : null,
   },
   cart: {
     cartItems: localStorage.getItem('cartItems')
@@ -24,6 +27,7 @@ const reducer = combineReducers({
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userSignin: userSigninReducer,
+  userRegister: userRegisterReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
